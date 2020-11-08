ac0cc897863004ae977f9875febbf3c73daa21e4
erixatech <erixatech@gmail.com>
2020-11-08 18:03:58 +0530

    changes1

--- express/server.js
+++ express/server.js
@@ -186,6 +186,27 @@ const handleUndo = async function (req, res) {
 	}
 }
 
+const neww = async function(myParameters) {
+    this.uriObj = await Aki.prototype.getSession();
+    this.uid = this.uriObj.uid;
+    this.frontaddr = this.uriObj.frontaddr;
+    console.log(`https://${this.uri}/new_session?callback=${jQuery + new Date().getTime()}&urlApiWs=https://${this.urlApiWs}/ws&partner=1&player=website-desktop&uid_ext_session=${this.uid}&frontaddr=${this.frontaddr}&constraint=ETAT%%3C%%3E%%27AV%%27&constraint=ETAT<>'AV'`);
+    const result = await request(`https://${this.uri}/new_session?callback=${jQuery + new Date().getTime()}&urlApiWs=https://${this.urlApiWs}/ws&partner=1&player=website-desktop&uid_ext_session=${this.uid}&frontaddr=${this.frontaddr}&constraint=ETAT%%3C%%3E%%27AV%%27&constraint=ETAT<>'AV'`);
+    const { body, statusCode } = result;
+
+    if (!statusCode || statusCode !== 200 || !body || body.completion !== 'OK' || !body.parameters || !body.parameters.step_information.question) {
+        akinatorAPIErrors(body, this.region);
+        return;
+    }
+
+    this.session = body.parameters.identification.session;
+    this.signature = body.parameters.identification.signature;
+    this.question = body.parameters.step_information.question;
+    this.challenge_auth = body.parameters.identification.challenge_auth;
+    this.answers = body.parameters.step_information.answers.map(ans => ans.answer);
+}
+
+
 const start = async function(req, res, region, userCode) {
 	try {
         console.log(req.method);
@@ -218,7 +239,7 @@ const start = async function(req, res, region, userCode) {
         }
         console.log(JSON.stringify(req.headers));
         console.log("Response headers : ");
-        console.log(res);
+        //console.log(res);
         console.log(req.method);
         const childMode = true;
         let aki = new Aki(region, childMode);
@@ -263,7 +284,7 @@ const start = async function(req, res, region, userCode) {
         console.log("Request2 headers : ");
         console.log(JSON.stringify(req.headers));
         console.log("Response2 headers : ");
-        console.log(res);
+        //console.log(res);
 		res.send(errorPrefix + "Server down or Technical Error. Please, Try again.");
 	}
 }
